a5828b9402b111421c49dc5ed1f7dcf0967d4c75
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2024-01-24 15:08:40 -0500

    button links

--- lockmasterpreferences/LockMasterPrefsRootListController.m
+++ lockmasterpreferences/LockMasterPrefsRootListController.m
@@ -11,4 +11,12 @@
 	return _specifiers;
 }
 
+- (void)openGithub {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/leminlimez/Lock-Master"] options:@{} completionHandler:nil];
+}
+
+- (void)openTwitter {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/LeminLimez"] options:@{} completionHandler:nil];
+}
+
 @end
