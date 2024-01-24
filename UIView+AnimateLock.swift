ebb9e14029f222dc774176f80576b87e11d04520
leminlimez <jirizarry317@gmail.com>
2024-01-24 12:10:35 -0500

    extend the official fade duration

--- UIView+AnimateLock.swift
+++ UIView+AnimateLock.swift
@@ -24,10 +24,10 @@ import Foundation
 import UIKit
 
 @objc(UIView) extension UIView {
-    @objc open func animateLock(_ animType: AnimationType = AnimationType.shrink, duration: Double = 0.5,
+    @objc open func animateLock(_ animType: AnimationType = AnimationType.shrink, duration: Double = 0.5, extendFadeBy: Double = 0.2,
         completion: (() -> ())? = nil) {
             DispatchQueue.main.async {
-                self.layer.animateLock(animType: animType, duration: duration, completion: completion)
+                self.layer.animateLock(animType: animType, duration: duration, fadeExtension: extendFadeBy, completion: completion)
             }
     }
 }
