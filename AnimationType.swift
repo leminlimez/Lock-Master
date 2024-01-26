ad451346ab07426b57e13e84b649a1bac285a28f
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2024-01-26 15:15:04 -0500

    tv no flash to white

--- AnimationType.swift
+++ AnimationType.swift
@@ -15,17 +15,18 @@ import Foundation
  The type of animation to play
  */
 @objc public enum AnimationType: Int {
-    case shrink
-    case expand
-    case slideLeft
-    case slideRight
-    case slideUp
-    case slideDown
-    case tv
-    case offBtnFadeInto
-    case offBtnFadeOut
-    case genie
-    case flip
-    case spinSlower
-    case spinFaster
+    case shrink             // id: 0
+    case expand             // id: 1
+    case slideLeft          // id: 2
+    case slideRight         // id: 3
+    case slideUp            // id: 4
+    case slideDown          // id: 5
+    case tv                 // id: 6
+    case offBtnFadeInto     // id: 7
+    case offBtnFadeOut      // id: 8 (Unused)
+    case genie              // id: 9
+    case flip               // id: 10
+    case spinSlower         // id: 11
+    case spinFaster         // id: 12
+    case tvNoFadeToWhite    // id: 13
 }
